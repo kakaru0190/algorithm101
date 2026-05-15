a8711993f4125463cf920a53ab1e2e41d300cc07
YS <kakaru0190@gmail.com>
2026-05-15 17:00:51 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.74 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -0,0 +1,36 @@
+function solution(n, wires) {
+    let answer = Infinity
+
+    const dfs = (node, graph, visited) => {
+        visited[node] = true
+        let count = 1
+
+        for (const next of graph[node]) {
+            if (visited[next]) continue
+
+            count += dfs(next, graph, visited)
+        }
+
+        return count
+    }
+
+    for (let cut = 0; cut < wires.length; cut++) {
+        const graph = Array.from({length: n + 1}, () => [])
+
+        for (let i = 0; i < wires.length; i++) {
+            if (i === cut) continue
+
+            const [a, b] = wires[i]
+            graph[a].push(b)
+            graph[b].push(a)
+        }
+
+        const visited = Array(n + 1).fill(false)
+        const count = dfs(1, graph, visited)
+
+        const diff = Math.abs(count - (n - count))
+        answer = Math.min(answer, diff)
+    }
+
+    return answer
+}
