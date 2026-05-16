11db744087231ee883799d6f9090e417d5ffda27
YS <kakaru0190@gmail.com>
2026-05-17 01:55:47 +0900

    [level 2] Title: 올바른 괄호, Time: 4.27 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    const stack = []
+
+    for (const char of s) {
+        if (char === '(') {
+            stack.push(char)
+            continue
+        }
+
+        if (stack.length === 0) {
+            return false
+        }
+
+        stack.pop()
+    }
+
+    return stack.length === 0
+}
