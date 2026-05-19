5ab1e920ef76e3de9485cd7159a5f7f2780218f9
YS <kakaru0190@gmail.com>
2026-05-20 01:44:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 29.08 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,15 @@
+function solution(participant, completion) {
+    const participantMap = new Map()
+
+    for (const name of participant) {
+        participantMap.set(name, (participantMap.get(name) ?? 0) + 1)
+    }
+
+    for (const name of completion) {
+        participantMap.set(name, participantMap.get(name) - 1)
+    }
+
+    for (const [name, count] of participantMap) {
+        if (count > 0) return name
+    }
+}
