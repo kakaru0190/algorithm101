e33a7e7360addf8c7b7d5dae5d3f88dd659acca1
YS <kakaru0190@gmail.com>
2026-06-01 01:12:02 +0900

    [level 2] Title: 가장 큰 수, Time: 147.00 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,9 +1,14 @@
 function solution(numbers) {
-    const result = numbers.sort((a, b) => {
+    const sortedNumbers = [...numbers].sort((a, b) => {
         const ab = `${a}${b}`
         const ba = `${b}${a}`
+
         return ba.localeCompare(ab)
     })
-    
-    return result.every((v) => v === 0) ? "0" : result.join('')
+
+    if (sortedNumbers[0] === 0) {
+        return '0'
+    }
+
+    return sortedNumbers.join('')
 }
