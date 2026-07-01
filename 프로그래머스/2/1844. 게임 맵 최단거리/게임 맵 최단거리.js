3144dd645e85a09eefc98b1309b6b029e73b6ecb
YS <kakaru0190@gmail.com>
2026-07-02 01:56:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 18.76 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,25 +1,35 @@
 function solution(maps) {
     const n = maps.length
     const m = maps[0].length
+    const visited = Array.from({length: n}, () => Array(m).fill(false))
+    const queue = [[0, 0, 1]]
 
-    const queue = [[0, 0]]
     let head = 0
+
+    visited[0][0] = true
 
     const dx = [1, -1, 0, 0]
     const dy = [0, 0, 1, -1]
 
     while (head < queue.length) {
-        const [x, y] = queue[head++]
-        
+        const [x, y, distance] = queue[head++]
+
+        if (x === n - 1 && y === m - 1) {
+            return distance
+        }
+
         for (let i = 0; i < 4; i++) {
             const nx = x + dx[i]
             const ny = y + dy[i]
+
             if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue
-            if (maps[nx][ny] !== 1) continue
-            maps[nx][ny] = maps[x][y] + 1
-            queue.push([nx, ny])
+            if (maps[nx][ny] === 0) continue
+            if (visited[nx][ny]) continue
+
+            visited[nx][ny] = true
+            queue.push([nx, ny, distance + 1])
         }
     }
 
-    return maps[n - 1][m - 1] === 1 ? -1 : maps[n - 1][m - 1]
+    return -1
 }
