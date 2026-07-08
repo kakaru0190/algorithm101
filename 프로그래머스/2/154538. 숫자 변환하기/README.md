1f18fa7fea60f50366e8c20a73c7e933403f04c9
YS <kakaru0190@gmail.com>
2026-07-08 12:07:38 +0900

    [level 2] Title: 숫자 변환하기, Time: 176.64 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/README.md
+++ 프로그래머스/2/154538. 숫자 변환하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 변환하기 - 154538 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538) 
 
 ### 성능 요약
 
-메모리: 149 MB, 시간: 239.36 ms
+메모리: 159 MB, 시간: 176.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 08일 17:24:57
+2026년 07월 08일 12:07:34
 
 ### 문제 설명
 
--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,30 +1,30 @@
 function solution(x, y, n) {
-    const queue = [[x, 0]];
-    const visited = new Set([x]);
-    
-    let head = 0;
-    
-    while (head < queue.length) {
-        const [current, count] = queue[head++];
-        
-        if (current === y) {
-            return count;
+    const queue = [[x, 0]]
+    const visited = new Set([x])
+
+    let head = 0
+
+    while(head < queue.length) {
+        const [current, count] = queue[head++]
+
+        if(current === y) {
+            return count
         }
-        
+
         const nextNumbers = [
             current + n,
             current * 2,
-            current * 3,
-        ];
-        
-        for (const next of nextNumbers) {
-            if (next > y) continue;
-            if (visited.has(next)) continue;
-            
-            visited.add(next);
-            queue.push([next, count + 1]);
+            current * 3
+        ]
+
+        for(const nextNumber of nextNumbers) {
+            if(nextNumber > y) continue
+            if(visited.has(nextNumber)) continue
+
+            visited.add(nextNumber)
+            queue.push([nextNumber, count + 1])
         }
     }
-    
-    return -1;
+
+    return -1
 }
