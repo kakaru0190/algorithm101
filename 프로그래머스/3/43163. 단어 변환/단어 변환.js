369ea85c1d07bf79d9702f08ce7e64dc2d0a53b9
YS <kakaru0190@gmail.com>
2026-07-12 15:27:31 +0900

    [level 3] Title: 단어 변환, Time: 0.43 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,44 +1,42 @@
-function isOneCharDiff(currWord, nextWord) {
+function isOneCharDiff(current, next) {
     let count = 0
 
-    for (let i = 0; i < currWord.length; i++) {
-        if (nextWord[i] !== currWord[i]) {
+    for (let i = 0; i < current.length; i++) {
+        if(current[i] !== next[i]) {
             count += 1
         }
-        if (count > 1) {
+
+        if(count > 1) {
             return false
         }
     }
 
-    return count === 1;
+    return true
 }
 
-// 시간복잡도 O(N X N X M)
 function solution(begin, target, words) {
-    if (!words.includes(target)) {
-        return 0;
+    if(!words.includes(target)) {
+        return 0
     }
 
-    let head = 0;
-    const queue = [[begin, 0]];
+    const queue = [[begin, 0]]
     const visited = new Set([begin])
+    let head = 0
 
-    while (head < queue.length) {
-        const [currentWord, count] = queue[head++]
+    while(head < queue.length) {
+        const [current, count] = queue[head++]
 
-        if (target === currentWord) {
+        if(current === target) {
             return count
         }
 
-        for (const nextWord of words) {
-            if (visited.has(nextWord)) continue
+        for(const nextWord of words) {
+            if(visited.has(nextWord)) continue
 
-            if (isOneCharDiff(currentWord, nextWord)) {
-                visited.add(nextWord);
+            if(isOneCharDiff(current, nextWord)) {
+                visited.add(nextWord)
                 queue.push([nextWord, count + 1])
             }
         }
     }
-
-    return 0
 }
