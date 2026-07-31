7a79f46eb8daeff8de4d5b22bc5c7ed1965b23b3
YS <kakaru0190@gmail.com>
2026-07-31 21:52:33 +0900

    Time: 2 ms (45.2%), Space: 60.8 MB (54.4%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.ts
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isBalanced(root: TreeNode | null): boolean {
+    const getHeight = (node: TreeNode | null): number => {
+        if (node === null) {
+            return 0
+        }
+
+        const leftHeight = getHeight(node.left)
+
+        if (leftHeight === -1) {
+            return -1
+        }
+
+        const rightHeight = getHeight(node.right)
+
+        if(rightHeight === -1) {
+            return -1
+        }
+
+        if(Math.abs(leftHeight - rightHeight) > 1) {
+            return -1
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1
+    }
+
+    return getHeight(root) !== -1
+}
