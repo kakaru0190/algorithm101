55bc1375ae6598f56d587bc0d19368264dc0c0e8
YS <kakaru0190@gmail.com>
2026-08-28 22:31:29 +0900

    Time: 42 ms (85.06%), Space: 67 MB (82.47%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.ts
+++ 0542-01-matrix/0542-01-matrix.ts
@@ -1,37 +1,38 @@
 function updateMatrix(mat: number[][]): number[][] {
-    const queue: [number, number][] = []
-    const result: number[][] = mat.map((yPoints) => yPoints.map((x) => x === 0 ? 0 : -1))
+  const queue: [number, number][] = [];
+  const result: number[][] = mat.map((y) => y.map((x) => (x === 0 ? 0 : -1)));
 
-    let head = 0
-
-    for (let y = 0; y < mat.length; y++) {
-        for (let x = 0; x < mat[0].length; x++) {
-            if (result[y][x] === 0) {
-                queue.push([y, x])
-            }
-        }
+  for (let y = 0; y < mat.length; y++) {
+    for (let x = 0; x < mat[0].length; x++) {
+      if (mat[y][x] === 0) {
+        queue.push([y, x]);
+      }
     }
+  }
 
-    const dx = [1, -1, 0, 0]
-    const dy = [0, 0, 1, -1]
+  let head = 0;
 
-    while (head < queue.length) {
-        const [y, x] = queue[head++]
+  const dx = [1, -1, 0, 0];
+  const dy = [0, 0, 1, -1];
 
-        for (let i = 0; i < 4; i++) {
-            const ny = dy[i] + y
-            const nx = dx[i] + x
+  while (head < queue.length) {
+    const [y, x] = queue[head++];
 
-            const isOutOfBounds = ny < 0 || ny >= mat.length || nx < 0 || nx >= mat[0].length
+    for (let i = 0; i < 4; i++) {
+      const ny = y + dy[i];
+      const nx = x + dx[i];
 
-            if(isOutOfBounds || result[ny][nx] !== -1) {
-                continue
-            }
+      const isOutOfBounds =
+        ny < 0 || nx < 0 || ny >= mat.length || nx >= mat[0].length;
 
-            result[ny][nx] = result[y][x] + 1
-            queue.push([ny, nx])
-        }
+      if (isOutOfBounds || result[ny][nx] !== -1) {
+        continue;
+      }
+
+      result[ny][nx] = result[y][x] + 1;
+      queue.push([ny, nx]);
     }
+  }
 
-    return result  
+  return result;
 };
